43b787aacb60ae670fc2dcf12028b15253849ae0
xKomaar <jakubo148@gmail.com>
2023-12-27 18:57:48 +0100

    Initial commit

---
+++ app/src/main/java/pl/sm_projekt_aplikacjatodo/MainActivity.java
@@ -0,0 +1,14 @@
+package pl.sm_projekt_aplikacjatodo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
