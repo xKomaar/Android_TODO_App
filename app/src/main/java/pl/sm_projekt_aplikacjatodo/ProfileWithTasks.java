0c4bc31a645a0b47d6ee2eeb7fa8ba7df1bc8ea6
xKomaar <jakubo148@gmail.com>
2024-01-09 13:16:59 +0100

    gettery settery

--- app/src/main/java/pl/sm_projekt_aplikacjatodo/ProfileWithTasks.java
+++ app/src/main/java/pl/sm_projekt_aplikacjatodo/ProfileWithTasks.java
@@ -10,4 +10,20 @@ public class ProfileWithTasks {
     public Profile profile;
     @Relation(parentColumn = "profileId", entityColumn = "taskOwnerId")
     public List<Task> taskList;
+
+    public Profile getProfile() {
+        return profile;
+    }
+
+    public void setProfile(Profile profile) {
+        this.profile = profile;
+    }
+
+    public List<Task> getTaskList() {
+        return taskList;
+    }
+
+    public void setTaskList(List<Task> taskList) {
+        this.taskList = taskList;
+    }
 }
