f4dd2b72b0989745235958450305fff0cf891d0f
xKomaar <jakubo148@gmail.com>
2024-01-27 16:44:47 +0100

    Wyszukiwarka tasków po nazwie

--- app/src/main/java/pl/sm_projekt_aplikacjatodo/activities/TaskListActivity.java
+++ app/src/main/java/pl/sm_projekt_aplikacjatodo/activities/TaskListActivity.java
@@ -15,11 +15,15 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
+import android.widget.ImageButton;
+import android.widget.PopupMenu;
+import androidx.appcompat.widget.SearchView;
 import android.widget.TextView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import pl.sm_projekt_aplikacjatodo.R;
 import pl.sm_projekt_aplikacjatodo.database.TaskRepository;
@@ -29,12 +33,12 @@ public class TaskListActivity extends AppCompatActivity {
     private TaskRepository taskRepository;
     private Menu menu;
 
+    private List<Task> tasks;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task_list);
-
-
 
         RecyclerView recyclerView = findViewById(R.id.recyclerView);
         final TaskAdapter taskAdapter = new TaskAdapter();
@@ -50,8 +54,61 @@ public class TaskListActivity extends AppCompatActivity {
             startActivity(intent);
         });
 
+        SearchView searchView = findViewById(R.id.searchView);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                String query = newText.toLowerCase();
+
+                List<Task> filteredTasks = tasks.stream()
+                        .filter(task -> task.getTitle().toLowerCase().contains(query))
+                        .collect(Collectors.toList());
+
+                taskAdapter.setTasks(filteredTasks);
+
+                return true;
+            }
+        });
+
+        ImageButton filterButton = findViewById(R.id.filterButton);
+        filterButton.setOnClickListener(view -> {
+            PopupMenu popupMenu = new PopupMenu(TaskListActivity.this, view);
+            MenuInflater inflater = popupMenu.getMenuInflater();
+            inflater.inflate(R.menu.task_list_filter_menu, popupMenu.getMenu());
+
+            popupMenu.setOnMenuItemClickListener(item -> {
+                // Handle filter options
+                switch (item.getItemId()) {
+                    case 1: //Sort by Date (ASC)
+                        // Implement sorting by date ascending
+                        return true;
+                    case 2: //Sort by Date (DESC)
+                        // Implement sorting by date descending
+                        return true;
+                    case 3: //Sort by Name (ASC)
+                        // Implement sorting by name ascending
+                        return true;
+                    case 4: //Sort by Name (DESC)
+                        // Implement sorting by name descending
+                        return true;
+                    default:
+                        return false;
+                }
+            });
+
+            popupMenu.show();
+        });
+
         taskRepository = new TaskRepository(this.getApplication());
-        taskRepository.findAllByTaskOwnerId(getIntent().getIntExtra("profileId", -1)).observe(this, taskAdapter::setTasks);
+        taskRepository.findAllByTaskOwnerId(getIntent().getIntExtra("profileId", -1)).observe(this, taskList -> {
+            this.tasks = taskList;
+            taskAdapter.setTasks(taskList);
+        });
     }
 
     private class TaskHolder extends RecyclerView.ViewHolder{
